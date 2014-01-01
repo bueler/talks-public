50d03e3b86b03a207a1e15da754dda6cb94880cc
Ed Bueler <elbueler@alaska.edu>
2014-01-01 10:56:46 -0900

    now all three types of figures are generated; need to write script that actually solves the various 1D FEM cases and calls the genfig.py functions to make the three figures for that case

--- S14DMScolloq/code/genfig.py
+++ S14DMScolloq/code/genfig.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 def convex2var(psi,u,center,name):
     '''Figure showing convex set K and solution in a 2-variable slice.'''
@@ -11,7 +12,6 @@ def convex2var(psi,u,center,name):
     def level(x,lev):
         return lev * np.ones(np.shape(x))
     # axes in black
-    plt.xkcd()
     x = np.linspace(-0.2,1.2,141)
     y = x.copy()
     plt.plot(x,getzero(x),'k')
@@ -21,9 +21,9 @@ def convex2var(psi,u,center,name):
     # constraints in color
     plt.plot(x[x>=psi[0]],level(x[x>=psi[0]],psi[1]),'g',lw=4.0)
     plt.plot(level(y[y>=psi[1]],psi[0]),y[y>=psi[1]],'r',lw=4.0)
-    plt.text(psi[0]-0.02,-0.1,r'$\psi_1$',fontsize=20)
+    plt.text(psi[0]-0.02,-0.1,r'$\psi_1$',fontsize=20,color='r')
     plt.plot([psi[0],psi[0]],[-0.02,0.02],'k')
-    plt.text(-0.1,psi[1],r'$\psi_2$',fontsize=20)
+    plt.text(-0.1,psi[1],r'$\psi_2$',fontsize=20,color='g')
     plt.plot([-0.02,0.02],[psi[1],psi[1]],'k')
     # contour lines dashed black on circles around (0.5,-0.1)
     theta = np.linspace(0.0,2.0*np.pi,201)
@@ -72,20 +72,56 @@ def oneD(x,psi,u,name):
     plt.text(0.9,-0.1,r'$\psi$',fontsize=20)
     # solution as stronger black
     plt.plot(x,u,'k',lw=4.0)
+    plt.plot(x[1:4],u[1:4],'ko',markersize=9)
     plt.text(0.1,0.15,r'$u$',fontsize=20)
     plt.axis('off')
     if name != None:
         plt.savefig(name)
-    
+
+def constraints3D(u,psi,myfig):
+    assert len(u)==5
+    assert len(psi)==5
+    ax = myfig.gca(projection='3d')
+    def linetoplane(j,x,y,z,c):
+        if u[j]-psi[j]>0.05:
+             ax.plot(np.array(x),np.array(y),np.array(z),'%c-' % c,lw=2.0,alpha=0.8)
+    linetoplane(1, [psi[1],u[1]-0.03], [u[2],u[2]],        [u[3],u[3]],        'r')
+    linetoplane(2, [u[1],u[1]],        [psi[2],u[2]-0.03], [u[3],u[3]],        'g')
+    linetoplane(2, [u[1],u[1]],        [u[2],u[2]],        [psi[3],u[3]-0.03], 'b')
+    ax.plot(u[1:2],u[2:3],u[3:4],'ko',markersize=12)
+    A = np.array([[1.0, 1.0], [1.0, 1.0]])
+    B = np.array([[0.0, 1.0], [0.0, 1.0]])
+    C = np.array([[0.0, 0.0], [1.0, 1.0]])
+    def scsh(P,t):  # scale and shift
+        return (1.0-t)*P+t
+    alf = 0.6
+    ax.plot_surface(psi[1]*A,scsh(B,psi[2]),scsh(C,psi[3]),color='r',alpha=alf)
+    ax.set_xticks((psi[1],))
+    ax.set_xticklabels((r'$\psi_1$',), color='r', fontsize=16)
+    ax.plot_surface(scsh(C,psi[1]),psi[2]*A,scsh(B,psi[3]),color='g',alpha=alf)
+    ax.set_yticks((psi[2],))
+    ax.set_yticklabels((r'$\psi_2$',), color='g', fontsize=16)
+    ax.plot_surface(scsh(B,psi[1]),scsh(C,psi[2]),psi[3]*A,color='b',alpha=alf)
+    ax.set_zticks((psi[3],))
+    ax.set_zticklabels((r'$\psi_3$',), color='b', fontsize=16)
+    ax.view_init(azim=50.0, elev=27.0)
+    ax.set_xlim3d(0.0,1.0)
+    ax.set_ylim3d(0.0,1.0)
+    ax.set_zlim3d(0.0,1.0)
+    #ax.set_xlabel(r'$u_1$', fontsize=16)
+    #ax.set_ylabel(r'$u_2$', fontsize=16)
+    #ax.set_zlabel(r'$u_3$', fontsize=16)
+
 if __name__ == '__main__':
-    fig1 = plt.figure()
-    #convex2var([0.1,0.3], [0.4,0.3], [0.5,-0.1], 'convexvar.png')
-    convex2var([0.1,0.3], [0.4,0.3], [0.5,-0.1], None)
-    fig2 = plt.figure()
-    psi = np.array([-0.2,0.1,0.2,0.3,-0.12])
     x = np.array([0.0, 0.25, 0.5, 0.75, 1.0])
+    psi = np.array([-0.2,0.1,0.2,0.3,-0.12])
     u = np.array([0.0, 0.2, 0.35, 0.4, 0.0])   # FIXME: this could come from FEM calculation
-    #oneD(x,psi,u,'oned-basecase.png')
+    fig1 = plt.figure()
+    with plt.xkcd():
+        convex2var(psi[1:3],u[1:3], [0.5,-0.1], None)
+    fig2 = plt.figure()
     oneD(x,psi,u,None)
+    fig3 = plt.figure()
+    constraints3D(u,psi,fig3)
     plt.show()
 
