dc977fa7728f048968bef79ae502c497bf7a97b7
Ed Bueler <elbueler@alaska.edu>
2014-01-11 21:21:30 -0900

    quite a bit of talk: abstract projection figures and cleaned up calc 1 figures and a "progress so far" slide mechanism

--- S14DMScolloq/code/abstractfigs.py
+++ S14DMScolloq/code/abstractfigs.py
@@ -10,26 +10,30 @@ def convexproj(x,y,xstr,ystr,num,**kwargs):
     plt.text(3.2,2.5,r'$K$',fontsize=28,color='b')
     plt.plot([x[0],x[1]], [y[0],y[1]], 'k.', markersize=18)
     plt.text(x[0]-0.1,y[0]+0.2,xstr,fontsize=22,color='k')
-    plt.text(x[1]-0.1,y[1]+0.3,ystr,fontsize=22,color='k')
     eta = kwargs.get('eta',None)
     if eta != None:
+        plt.text(x[1]-0.1,y[1]+0.3,ystr,fontsize=22,color='k')
         ax = plt.axes()
         ax.arrow(x[0], y[0], x[1]-x[0], y[1]-y[0],
                  head_width=0.1, head_length=0.2, length_includes_head=True, fc='k', ec='k')
+        plt.plot([x[1],x[1]+0.4*(x[1]-x[0])],[y[1],y[1]+0.4*(y[1]-y[0])],'k--',lw=2.0)
         plt.plot([eta[0],], [eta[1],], 'k.', markersize=18)
         plt.text(eta[0]+0.1,eta[1]+0.1,r"$\eta$",fontsize=22,color='k')
-        # FIXME: add arrow to eta
+        ax.arrow(x[1], y[1], eta[0]-x[1], eta[1]-y[1],
+                 head_width=0.1, head_length=0.2, length_includes_head=True, fc='k', ec='k')
+    else:
+        plt.text(x[1]-1.0,y[1]+0.3,ystr,fontsize=22,color='k')
     plt.axis([-1.5,4.0,0.0,5.0],'k',lw=2.0)
     plt.axis('off')
     return fig
 
 if __name__ == '__main__':
     #plt.xkcd()
-    convexproj([-1.0,1.0],[2.4,2.0],r"$x$",r"$y$",1)
+    convexproj([-1.0,1.0],[2.4,2.0],r"$x$",r"$y=\Pi_K x$",1)
     plt.savefig('proj_xy.png')
-    convexproj([0.4,1.5],[3.7,3.0],r"$x'$",r"$y'$",2)
-    plt.savefig('proj_xyprime.png')
-    convexproj([-1.0,1.0],[2.4,2.0],r"$x$",r"$y$",3,eta=[2.0,3.5])
+    convexproj([-1.0,1.0],[2.4,2.0],r"$x$",r"$y$",2,eta=[2.0,3.5])
     plt.savefig('proj_xy_vect.png')
+    convexproj([0.4,1.5],[3.7,3.0],r"$x'$",r"$y'$",3,eta=[2.0,3.5])
+    plt.savefig('proj_xyprime_vect.png')
     #plt.show()
 
--- S14DMScolloq/code/calcfigs.py
+++ S14DMScolloq/code/calcfigs.py
@@ -10,18 +10,18 @@ def prepaxes(num,**kwargs):
     yy = np.linspace(-1.0,5.0,7)
     plt.plot(xx,getzero(xx),'k')
     plt.plot(getzero(yy),yy,'k')
-    plt.text(5.5,-0.1,r'$x$',fontsize=20)
-    plt.text(-0.1,5.5,r'$y$',fontsize=20)
+    plt.text(5.5,-0.1,r'$x$',fontsize=28)
+    plt.text(-0.1,5.5,r'$y$',fontsize=28)
     plt.axis([-0.5,5.5,-1.5,5.5])
     plt.axis('off')
     a = kwargs.get('a',None)
     if a != None:
         plt.plot([a,a],[-0.1,0.1],'k')
-        plt.text(a-0.1,-0.5,r'$a$',fontsize=20)
+        plt.text(a-0.1,-0.5,r'$a$',fontsize=28)
     b = kwargs.get('b',None)
     if b != None:
         plt.plot([b,b],[-0.1,0.1],'k')
-        plt.text(b-0.1,-0.5,r'$b$',fontsize=20)
+        plt.text(b-0.1,-0.5,r'$b$',fontsize=28)
     return fig
 
 def calcprob(root):
@@ -30,7 +30,7 @@ def calcprob(root):
     x = np.linspace(-0.4,5.0,301)
     f = - 0.7*x + 4.0 + 1.3*np.cos(x)**3
     plt.plot(x,f)
-    plt.text(2.0,3.0,r'$f$',fontsize=20)
+    plt.text(2.0,3.0,r'$f$',fontsize=28)
     if root != None:
         plt.savefig(root + 'bare.png')
     # figure with closed interval and soln
@@ -40,44 +40,56 @@ def calcprob(root):
     x = x[(x<=b)&(x>=a)]
     prepaxes(2,a=a,b=b)
     plt.plot(x,f)
-    plt.text(2.0,3.0,r'$f$',fontsize=20)
+    plt.text(2.0,3.0,r'$f$',fontsize=28)
     ic = mlab.find(f==min(f))[0]
     u = x[ic]
     fu = f[ic]
     plt.plot(u,fu,'k.',markersize=18)
-    plt.plot([u,u],[-0.15,0.15],'k')
-    plt.text(u-0.1,-0.7,r'$u$',fontsize=24)
+    plt.plot([u,u],[-0.1,0.1],'k')
+    plt.text(u-0.1,-0.7,r'$u$',fontsize=32)
     if root != None:
         plt.savefig(root + 'soln.png')
     # figure with closed interval and soln and tangent line
     prepaxes(3,a=a,b=b)
     plt.plot(x,f)
-    plt.text(2.0,3.0,r'$f$',fontsize=20)
+    plt.text(2.0,3.0,r'$f$',fontsize=28)
     plt.plot(u,fu,'k.',markersize=18)
-    plt.plot([u,u],[-0.15,0.15],'k')
-    plt.text(u-0.1,-0.7,r'$u$',fontsize=24)
+    plt.plot([u,u],[-0.1,0.1],'k')
+    plt.text(u-0.1,-0.7,r'$u$',fontsize=32)
     plt.plot([u-0.6,u+0.6],[fu,fu],'k--',linewidth=2.5)
     if root != None:
         plt.savefig(root + 'soln_interior.png')
-    # figure with closed interval and soln and tangent line (at endpoint)
+    # figure with closed interval and soln and tangent line (at LEFT endpoint)
     prepaxes(4,a=a,b=b)
+    x = np.linspace(a,b,301)
+    f = 5.0 - (- 0.7*x + 4.0 + 1.3*np.cos(x)**3)
+    plt.plot(x,f)
+    plt.text(2.0,3.5,r'$f$',fontsize=28)
+    plt.plot(a,f[0],'k.',markersize=18)
+    plt.text(a-0.2,-1.0,r'$=u$',fontsize=32)
+    t = np.linspace(a,a+0.8,2)
+    fa = f[0]
+    dfa = + 0.7 - 1.3 * np.cos(a)**2 * np.sin(a)
+    plt.plot(t,fa+dfa*(t-a),'k--',linewidth=3.5)
+    if root != None:
+        plt.savefig(root + 'soln_leftendpoint.png')
+    # figure with closed interval and soln and tangent line (at RIGHT endpoint)
+    prepaxes(5,a=a,b=b)
     x = np.linspace(a,b,301)
     f = - 0.7*x + 4.0 - 1.2*np.cos(x)
     plt.plot(x,f)
-    plt.text(2.0,3.5,r'$f$',fontsize=20)
+    plt.text(2.0,3.5,r'$f$',fontsize=28)
     plt.plot(b,f[-1],'k.',markersize=18)
-    plt.plot([b,b],[-0.15,0.15],'k')
-    plt.text(b-0.2,-1.0,r'$=u$',fontsize=24)
+    plt.text(b-0.2,-1.0,r'$=u$',fontsize=32)
     t = np.linspace(b-0.8,b,2)
     fb = f[-1]
     dfb = -0.7 + 1.2 * np.sin(b)
     plt.plot(t,fb+dfb*(t-b),'k--',linewidth=3.5)
     if root != None:
-        plt.savefig(root + 'soln_endpoint.png')
+        plt.savefig(root + 'soln_rightendpoint.png')
     # show if not saved
     if root == None:
         plt.show()
-
 
 if __name__ == '__main__':
     with plt.xkcd():
