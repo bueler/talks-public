3ffbb1104361be2d12cd291ee257aacd67f44b58
Ed Bueler <elbueler@alaska.edu>
2023-03-23 12:48:03 -0800

    fix figure so it works with python3

--- 2017/DMScolloq/figs/NPplane.py
+++ 2017/DMScolloq/figs/NPplane.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # consider N-P plane:
 #     N = number of unknowns
@@ -26,31 +26,31 @@ for j in range(3):
     ax = plt.gca()
 
     ax.arrow(-0.7, 0.0, 7.7, 0.0, head_width=0.07, head_length=0.15, fc='k', ec='k')
-    plt.text(4.0,-1.0,'$P$ = number of processors',fontsize=12.0)
+    plt.text(2.5,-0.9,'$P$ = number of processors',fontsize=12.0)
 
     ax.arrow(0.0, -1.0, 0.0, 7.0, head_width=0.07, head_length=0.15, fc='k', ec='k')
-    plt.text(-0.9,6.0,'$N$ = degrees of freedom',fontsize=12.0,rotation=90.0)
+    plt.text(-0.5,1.5,'$N$ = degrees of freedom',fontsize=12.0,rotation=90.0)
 
     plt.plot([1.0, 1.0],[-dash,dash],'k')
     plt.text(0.7,-0.4,'$P$=1',fontsize=12.0)
     plt.text(0.6,-0.7,'(serial)',fontsize=12.0)
 
     ax.arrow(1.0, 0.5, 0.0, 5.0, head_width=0.15, head_length=0.2, fc='w', ec='k')
-    plt.text(0.65,2.5,'optimality',fontsize=12.0,rotation=90.0)
+    plt.text(0.65,1.8,'optimality',fontsize=12.0,rotation=90.0)
 
     if j >= 1:
         plt.plot([5.0, 5.0],[-dash,dash],'k')
         ax.arrow(5.0, 0.5, 0.0, 5.0, head_width=0.15, head_length=0.2, fc='w', ec='k')
-        plt.text(4.7,2.8,'static scaling',fontsize=12.0,rotation=90.0)
+        plt.text(4.65,1.8,'static scaling',fontsize=12.0,rotation=90.0)
         plt.text(4.9,-0.5,'$\hat P$',fontsize=12.0)
 
     if j >= 2:
         ax.arrow(1.5, 1.5, 4.3, 4.3, head_width=0.15, head_length=0.2, fc='w', ec='k')
-        plt.text(1.5,3.4,'weak scaling (slope=1)',fontsize=12.0,rotation=44.0)
+        plt.text(1.5,1.2,'weak scaling (slope=1)',fontsize=12.0,rotation=44.0)
 
         plt.plot([-dash,dash],[4.0,4.0],'k')
         ax.arrow(1.3, 4.0, 5.5, 0.0, head_width=0.15, head_length=0.2, fc='w', ec='k')
-        plt.text(1.4,4.2,'strong scaling',fontsize=12.0)
+        plt.text(2.0,4.2,'strong scaling',fontsize=12.0)
         plt.text(-0.4,3.9,'$\hat N$',fontsize=12.0)
 
     plt.axis('off')
